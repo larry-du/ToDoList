cdea44a173ff2ccf1371c10b1ad4f5b3188d0fca
Larry <shaka422@hotmail.com>
2020-07-13 19:53:35 +0800

    功能 新增編輯後儲存與取消功能

--- Larry_T7.1/Vanilla JS/module.js
+++ Larry_T7.1/Vanilla JS/module.js
@@ -1,25 +1,22 @@
 // import { creatList } from './template/creatListTemplate.js'
-
-// console.log(creatList)
 //取得編輯頁面元素
 const eventAdd = document.querySelector('.event-add');
 //取得增加任務按鈕元素
 const addArea = document.querySelector('.add-area');
 const editSaveArea = document.querySelector('.save-area');
-// console.log(editSaveArea)
 //取得增加任務元素
 const addTask = document.querySelector('.add-task');
-const addButton = document.querySelector('#add');
-const addSchedule = document.querySelector('.add-schedule');
-const saveButton = document.querySelector('#save-button');
+const addButton = document.querySelector('#add-button');
+const toDoListArea = document.querySelector('.to-do-list-area');
 //取得localStorage資料,如無資料會是空陣列
-let items = JSON.parse(localStorage.getItem('items')) || [];
+let toDoListData = JSON.parse(localStorage.getItem('toDoListData')) || [];
+// console.log(toDoListData)
 
 //畫面渲染
 render();
 
 //開啟與關閉增加任務
-function toggleAddTaskArea(e) {
+function openAddTaskArea(e) {
     // console.log(e.target)
     if (eventAdd.matches('.event-area-block')) {
         eventAdd.classList.remove('event-area-block');
@@ -29,7 +26,7 @@ function toggleAddTaskArea(e) {
     eventAdd.classList.add('event-area-block');
 }
 
-function addTaskList(e) {
+function addTaskData(e) {
     //取得編輯頁面標題元素
     const title = document.querySelector('.type-tittle');
     // console.log(title.value)
@@ -49,11 +46,11 @@ function addTaskList(e) {
         id: Date.now()
     }
     //將資料上傳
-    items.push(allMessage);
+    toDoListData.push(allMessage);
     //畫面渲染
     render();
     //資料存進localStorage
-    localStorage.setItem('items', JSON.stringify(items));
+    localStorage.setItem('toDoListData', JSON.stringify(toDoListData));
     //輸入內容清空
     title.value = '';
     date.value = '';
@@ -66,17 +63,18 @@ function addTaskList(e) {
 }
 
 //動態新增的清單
-function creatList(item) {
-    return `<div class="task container">
+function creatList(data) {
+    return `
+<div class="task container">
     <div class="card">
         <div class="card-body">
             <div class="card-title">
                 <div class="check-area">
                     <input type="checkbox"
-                        id="task-${item.id}">
-                    <label for="task-${item.id}"></label>
+                        id="task-${data.id}">
+                    <label for="task-${data.id}"></label>
                 </div>
-                <span>${item.title}</span>
+                <input class="list-title" value="${data.title}" disabled>
             </div>
             <div class="edit-area">
                 <i class="fas fa-star important"></i>
@@ -87,13 +85,13 @@ function creatList(item) {
         <div class="status">
             <div class="date">
                 <i class="far fa-calendar-alt"></i>
-                <span>${item.date}</span>
+                <span>${data.date}</span>
             </div>
             <i class="fal fa-file"></i>
             <i class="fal fa-comment-dots"></i>
         </div>
     </div>
-    </div>`
+</div>`
 }
 
 //動態新增編輯清單
@@ -138,11 +136,11 @@ function creatEditList() {
         </div>
     </div>
     <div class="check-button">
-    <button>
+    <button id="cancel">
         <i class="fal fa-times"></i>
         Cancel
     </button>
-    <button id="save-button">
+    <button id="save">
         <i class="fal fa-plus"></i>
         Save
     </button>
@@ -152,161 +150,96 @@ function creatEditList() {
 }
 
 function render() {
-    addSchedule.innerHTML = items.map(item => {
-        return creatList(item)
+    toDoListArea.innerHTML = toDoListData.map(data => {
+        return creatList(data)
     }).join('');
     deleteData();
-    editSaveList();
+    editTaskList();
 }
 
-
 function deleteData() {
-    const taskList = document.querySelectorAll('.task');
-    const deleteButtons = document.querySelectorAll('.edit-area .trash');
-    const deleteButtonArray = [...deleteButtons];
+    const [...tasks] = document.querySelectorAll('.task');
+    const [...deleteButtons] = document.querySelectorAll('.edit-area .trash');
     if (deleteButtons.length !== 0) {
-        // console.log(deleteButtonArray)
-        deleteButtonArray.map((deleteButton, deleteButtonIndex) => {
+        // console.log(deleteButtons)
+        deleteButtons.map((deleteButton, deleteButtonIndex) => {
             deleteButton.addEventListener('click', () => {
                 // console.log(taskList, deleteButtonIndex)
                 console.log(deleteButton)
-                taskList[deleteButtonIndex].remove();
-                const updateData = items.filter((item, itemIndex) => itemIndex !== deleteButtonIndex);
-                localStorage.setItem('items', JSON.stringify(updateData));
+                tasks[deleteButtonIndex].remove();
+                const updateData = toDoListData.filter((item, itemIndex) => itemIndex !== deleteButtonIndex);
+                localStorage.setItem('toDoListData', JSON.stringify(updateData));
             });
         });
     }
 }
 
-function editSaveList() {
+function editTaskList() {
     const [...editButtons] = document.querySelectorAll('.task .edit-pen');
-    const saveTittle = document.querySelector('.save input[type="text"]');
-    const saveDate = document.querySelector('.save input[type="date"]');
-    const saveTime = document.querySelector('.save input[type="time"]');
-    const saveComment = document.querySelector('.save .comment-area');
-    // console.log(editButtons)
-    // function switchStatus() {
-    //     let isEdit = null;
-    //     return {
-    //         getStatus() {
-    //             return isEdit
-    //         },
-    //         switch() {
-    //             isEdit = !isEdit;
-    //         }
-    //     }
-    // }
-    // let editStatus = switchStatus();
-    const editPage = creatEditList();
     const [...tasks] = document.querySelectorAll('.task');
+
+    const editPage = creatEditList();
     editButtons.map((editButton, editButtonIndex) => {
-        //將Html結構插入
-        tasks[editButtonIndex].insertAdjacentHTML('beforeend', editPage);
+        const task = tasks[editButtonIndex];
+        task.insertAdjacentHTML('beforeend', editPage);
+
+        const taskDate = task.querySelector('.save-info input[type="date"]');
+        const taskTime = task.querySelector('.save-info input[type="time"]');
+        const taskComment = task.querySelector('.save-info .comment-area');
+        const taskTitle = task.querySelector('.list-title')
+        const saveButton = task.querySelector('#save');
+        const cancelButton = task.querySelector('#cancel');
+
         editButton.addEventListener('click', (e) => {
-            // //全域找save-block
-            // const prevSaveInfo = document.querySelector('.save-block');
-            // //被點擊的對象有save-info
-            // const currentSaveInfo = tasks[editButtonIndex].querySelector('.save-info')
-            // //被點擊的元素要加save-block , 如果原本有就移除
-            // currentSaveInfo.classList.toggle('save-block');
-            // //如果全域有save-block且不是點擊對象自己
-            // if (prevSaveInfo && prevSaveInfo !== currentSaveInfo) {
-            //     //全域找到的要移除
-            //     prevSaveInfo.classList.remove('save-block')
-            // }
+            taskTitle.disabled = !taskTitle.disabled;
+
+            taskDate.value = toDoListData[editButtonIndex].date;
+            taskTime.value = toDoListData[editButtonIndex].time;
+            taskComment.value = toDoListData[editButtonIndex].comment;
 
             const isEdit = document.querySelector('.isEdit');
-            const task = tasks[editButtonIndex];
-
             task.classList.toggle('isEdit');
             isEdit?.classList.remove('isEdit');
-            // if (isEdit) {
-            //     isEdit.classList.remove('isEdit');
-            // }
-
-
-            // if (isEdit) {
-            //     isEdit.classList.remove('isEdit')
-            // }
-            // // if (isEdit === task) {
-            // //     task.classList.add('isEdit')
-            // // }
-            // if (taskt.matches('.isEdit')) {
-            //     task.classList.remove('isEdit')
-            //     return
-            // }
-            // task.classList.add('isEdit')
-
-
-            // if (prevSaveInfo) {
-            //     currentSaveInfo.classList.toggle('save-block');
-            //     if (prevSaveInfo !== currentSaveInfo) {
-            //         prevSaveInfo.classList.remove('save-block')
-            //     }
-            // } else {
-            //     currentSaveInfo.classList.add('save-block');
-            // }
-
-
-            // let prevStatus = editStatus.getStatus();
-            // if (editStatus.getStatus() === null) {
-            //     // let creatEditPage = document.createRange().createContextualFragment(editPage);
-            //     // creatEditPage.querySelector('.save-info').classList.add('save-block');
-            //     tasks[editButtonIndex].insertAdjacentHTML('beforeend', editPage);
-            //     // console.log(editButtonIndex, document.querySelectorAll('.save-info'))
-
-            //     // const [...saveInfoNode] = document.querySelectorAll('.save-info');
-            //     // saveInfoNode[editButtonIndex].classList.add('save-block');
-            //     editStatus.switch();
-            //     console.log(editButtonIndex + 1, `${prevStatus} -> ${editStatus.getStatus()}`, document.querySelector('.save-info'))
-            //     return
-            // }
-            // if (editStatus.getStatus()) {
-            //     const saveInfo = document.querySelector('.save-info');
-            //     saveInfo.classList.remove('save-block');
-            //     editStatus.switch();
-
-            // } else {
-            //     const saveInfo = document.querySelector('.save-info');
-            //     saveInfo.classList.add('save-block');
-            //     editStatus.switch();
-            // }
-            // console.log(editButtonIndex + 1, `${prevStatus} -> ${editStatus.getStatus()}`, document.querySelector('.save-info'))
         })
+
+        saveButton.addEventListener('click', function (event) {
+            saveEdit(task, editButtonIndex);
+        });
+
+        cancelButton.addEventListener('click', function (event) {
+            cancelEdit();
+        });
     })
 }
 
-// function saveSaveList() {
-// // console.log('save')
-// // localStorage.setItem('items', JSON.stringify(items));
-// const taskList = document.querySelectorAll('.task');
-// const taskListArray = [...taskList]
-// const saveTittle = document.querySelector('.save input[type="text"]');
-// const saveDate = document.querySelector('.save input[type="date"]');
-// const saveTime = document.querySelector('.save input[type="time"]');
-// const saveComment = document.querySelector('.save .comment-area');
-// console.log(items)
-// // console.log(taskListArray)
-// // taskListArray.map((taskItem, taskItemIndex) => {
-// items[taskItemIndex].date = saveDate.value
-// items[taskItemIndex].time = saveTime.value
-// items[taskItemIndex].comment = saveComment.value
-// items[taskItemIndex].title = saveTittle.value
-// // })
-// render(items);
-// }
+function saveEdit(task, index) {
+    const taskTitle = task.querySelector('.task .list-title')
+    const taskDate = task.querySelector('.save-info input[type="date"]');
+    const taskTime = task.querySelector('.save-info input[type="time"]');
+    const taskComment = task.querySelector('.save-info .comment-area');
 
-// function toggleSaveArea(e) {
-//     // console.log(e.target)
-//     if (editSaveArea.matches('.save-area-block')) {
-//         editSaveArea.classList.remove('save-area-block')
-//         return
-//     };
-//     addTaskArea.classList.remove('event-area-block');
-//     editSaveArea.classList.add('save-area-block')
-// }
+    const allMessage = {
+        title: taskTitle.value,
+        date: taskDate.value,
+        time: taskTime.value,
+        comment: taskComment.value,
+        id: toDoListData[index].id
+    }
 
-addButton.addEventListener('click', addTaskList);
-addTask.addEventListener('click', toggleAddTaskArea);
+    toDoListData[index] = allMessage;
+    localStorage.setItem('toDoListData', JSON.stringify(toDoListData));
+
+    const isEdit = document.querySelector('.isEdit');
+    isEdit?.classList.remove('isEdit');
+
+}
+
+function cancelEdit() {
+    const isEdit = document.querySelector('.isEdit');
+    isEdit?.classList.remove('isEdit');
+}
+
+addButton.addEventListener('click', addTaskData);
+addTask.addEventListener('click', openAddTaskArea);
 // saveButton.addEventListener('click', saveSaveList)
 
