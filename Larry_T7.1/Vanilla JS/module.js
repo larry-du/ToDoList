96ebfc0b9739cd5204186cf7db69ca6558357029
Larry <shaka422@hotmail.com>
2020-07-22 10:23:18 +0800

    功能 complete按鈕功能完成

--- Larry_T7.1/Vanilla JS/module.js
+++ Larry_T7.1/Vanilla JS/module.js
@@ -1,25 +1,31 @@
 import { createTaskToLocalStorage, allData } from './model.js';
 import { creatList, creatEditList } from './template.js';
 const addTask = document.querySelector('.add-task');
-const addTaskButton = document.querySelector('#add-button');
+// const addTaskButton = document.querySelector('#add-button');
+const event = document.querySelector('.event')
+// console.log(event)
 const listArea = document.querySelector('.to-do-list-area');
+let currentTask = null;
 
-render()
+
+render(sortData());
 
 function addNewTask() {
     const addArea = document.querySelector('.add-area');
-    const newTask = document.querySelector('.event-add')
+    const newTask = document.querySelector('.event-add');
     addArea.classList.add('add-area-none');
     newTask.classList.add('event-area-block');
 }
 
-function addTaskData() {
+function addTaskData(event) {
     const addArea = document.querySelector('.add-area');
     const newTask = document.querySelector('.event-add');
     const title = document.querySelector('.event .type-title');
     const date = document.querySelector('.event input[type="date"]');
     const time = document.querySelector('.event input[type="time"]');
     const comment = document.querySelector('.event .comment-area');
+    const addTaskButton = document.querySelector('#add-button');
+    const cancelButton = document.querySelector('#cancel-button')
 
     const taskInfo = {
         title: title.value,
@@ -27,44 +33,60 @@ function addTaskData() {
         time: time.value,
         comment: comment.value
     }
-    createTaskToLocalStorage(taskInfo)
 
-    render()
+    if (event.target === addTaskButton) {
+        createTaskToLocalStorage(taskInfo);
 
-    title.value = '';
-    date.value = '';
-    time.value = '';
-    comment.value = '';
+        render(sortData());
 
-    addArea.classList.remove('add-area-none');
-    newTask.classList.remove('event-area-block');
+        title.value = '';
+        date.value = '';
+        time.value = '';
+        comment.value = '';
+
+        addArea.classList.remove('add-area-none');
+        newTask.classList.remove('event-area-block');
+    }
+
+    if (event.target === cancelButton) {
+        title.value = '';
+        date.value = '';
+        time.value = '';
+        comment.value = '';
+
+        addArea.classList.remove('add-area-none');
+        newTask.classList.remove('event-area-block');
+    }
+
 }
 
-function render() {
-    listArea.innerHTML = allData.map(data => creatList(data)).join('');
+function render(sortData) {
+    listArea.innerHTML = sortData.map(data => creatList(data)).join('');
     const [...tasks] = document.querySelectorAll('.task');
 
     const editPage = creatEditList();
     tasks.map(task => task.insertAdjacentHTML('beforeend', editPage));
-    eventBinding(allData);
+    editEventBinding(sortData);
 }
 
-function eventBinding(allData) {
-    allData.map(data => {
+function editEventBinding(sortData) {
+    sortData.map(data => {
         const taskElement = document.querySelector(`.task-${data.id}`)
         taskElement.addEventListener('click', event => {
-            taskEventBinding.call(taskElement, event, allData);
+            taskEditEventBinding.call(taskElement, event);
         })
     })
 }
 
-function taskEventBinding(event, allData) {
+function taskEditEventBinding(event) {
     const isStar = event.target.classList.contains('top-star');
     const isEdit = event.target.classList.contains('edit-pen');
     const isDelete = event.target.classList.contains('trash');
     const isSave = event.target.classList.contains('save');
     const isCancel = event.target.classList.contains('cancel');
 
+    // const label = this.querySelector('label');
+    const check = this.querySelector('input[type="checkbox"]');
     const title = this.querySelector('.list-title');
     const date = this.querySelector('input[type="date"]');
     const time = this.querySelector('input[type="time"]');
@@ -72,38 +94,61 @@ function taskEventBinding(event, allData) {
 
     // const taskClass = this.classList;
     // const classNumber = taskClass[1].slice(5);
-    const setNumber = this.dataset.number;
-    const dataIndex = allData.findIndex(data => data.id === Number(setNumber));
+    const taskNumber = this.dataset.number;
+    const dataIndex = allData.findIndex(data => data.id === Number(taskNumber));
+    const targetTask = allData[dataIndex];
+
+    if (event.target === check) {
+        targetTask.isComplete = !targetTask.isComplete;
+        localStorage.setItem('allMessage', JSON.stringify(allData))
+        render(sortData());
+    }
+    if (date.value) {
+        // console.log('abc')
+    }
 
     if (isStar) {
-        console.log(isComplete)
-        this.classList.toggle('high-light');
+        targetTask.isStar = !targetTask.isStar;
+        localStorage.setItem('allMessage', JSON.stringify(allData))
+        render(sortData());
+        // this.classList.toggle('high-light');
     }
+
     if (isEdit) {
         title.disabled = !title.disabled;
-        title.value = allData[dataIndex].title;
-        date.value = allData[dataIndex].date;
-        time.value = allData[dataIndex].time;
-        comment.value = allData[dataIndex].comment;
+        title.value = targetTask.title;
+        date.value = targetTask.date;
+        time.value = targetTask.time;
+        comment.value = targetTask.comment;
 
+        let preTask = currentTask
+        currentTask = targetTask.id;
         this.classList.toggle('isEdit');
+        if (preTask && preTask !== currentTask) {
+            const preTaskElement = document.querySelector(`[data-number="${preTask}"]`)
+            preTaskElement.classList.remove('isEdit');
+        }
+
     }
+
     if (isDelete) {
         allData.splice(dataIndex, 1);
         localStorage.setItem('allMessage', JSON.stringify(allData));
-        render();
+        render(sortData());
     }
+
     if (isSave) {
         const allMessage = {
             title: title.value,
             date: date.value,
             time: time.value,
             comment: comment.value,
-            id: allData[dataIndex].id
+            id: targetTask.id
         }
-        allData[dataIndex] = allMessage;
+        targetTask = allMessage;
         localStorage.setItem('allMessage', JSON.stringify(allData));
-        this?.classList.remove('isEdit');
+        render(sortData());
+        // this?.classList.remove('isEdit');
     }
 
     if (isCancel) {
@@ -112,18 +157,27 @@ function taskEventBinding(event, allData) {
         time.value = '';
         comment.value = '';
         this.classList.remove('isEdit');
-        render();
+        render(sortData());
     }
 }
 
+function sortData() {
+    // console.log(allData)
+    const hightLight = allData.filter(data => data.isStar);
+    // console.log(hightLight)
+    const normal = allData.filter(data => !data.isStar);
+
+    return [...hightLight, ...normal]
+}
 
 function mounted() {
     render()
-    eventBinding()
+    editEventBinding()
 }
 
 addTask.addEventListener('click', addNewTask);
-addTaskButton.addEventListener('click', addTaskData);
+// addTaskButton.addEventListener('click', addTaskData);
+event.addEventListener('click', (event) => addTaskData(event))
 // tasks.map(task => {
 //     task.addEventListener('click', controller)
 // });
--- Larry_T7.1/Vanilla JS/template.js
+++ Larry_T7.1/Vanilla JS/template.js
@@ -1,12 +1,12 @@
 function creatList(data) {
     return `
-<div class="task task-${data.id} container ${data.isEdit ? 'isEdit' : ''} ${data.isStar ? 'high-light' : ''} ${data.complete ? 'complete' : ''}" data-number="${data.id}">
+<div class="task task-${data.id} container ${data.isEdit ? 'isEdit' : ''} ${data.isStar ? 'high-light' : ''} ${data.isComplete ? 'is-complete' : ''}" data-number="${data.id}">
     <div class="card">
         <div class="card-body">
             <div class="card-title">
                 <div class="check-area">
                     <input type="checkbox"
-                        id="task-check-${data.id}" ${data.complete ? 'checked' : ''}>
+                        id="task-check-${data.id}" ${data.isComplete ? 'checked' : ''}>
                     <label for="task-check-${data.id}"></label>
                 </div>
                 <input class="list-title" value="${data.title}" disabled>
@@ -22,8 +22,8 @@ function creatList(data) {
                 <i class="far fa-calendar-alt"></i>
                 <span>${data.date}</span>
             </div>
-            <i class="fal fa-file"></i>
-            <i class="fal fa-comment-dots"></i>
+            <i class="fal fa-file file-icon"></i>
+            <i class="fal fa-comment-dots comment-icon"></i>
         </div>
     </div>
 </div>`
