f64483e1d3b2165575519599e62fd5a565296fe3
Larry <shaka422@hotmail.com>
2020-08-04 20:15:53 +0800

    功能 點擊重要按鈕 編輯按鈕 取消按鈕 樣式完成

--- Larry_T7.1/jQuery/module.js
+++ Larry_T7.1/jQuery/module.js
@@ -1,4 +1,4 @@
-const event = $('.event');
+// const event = $('.event');
 
 $('.add-area').on('click', openNewTask);
 
@@ -7,12 +7,33 @@ function openNewTask() {
     $('.event-add').addClass('event-area-block');
 }
 
-$(document).on('click', event, newEventBinding)
+$('.event').on('click', $('.event'), newEventBinding)
 
 function newEventBinding(e) {
     const isStar = $(e.target).hasClass('top-star');
     const isEdit = $(e.target).hasClass('edit-pen');
-    const isCancel=$(e.target).hasClass('cancel-button');
-    const isAddNewTask=$(e.target).hasClass('add-button');
-    
+    const isCancel = $(e.target).hasClass('cancel-button');
+    const isAddNewTask = $(e.target).hasClass('add-button');
+    console.log(this)
+    if (isStar) {
+        $(this).toggleClass('high-light');
+    }
+    if (isEdit) {
+        $('.add-area').removeClass('add-area-none');
+        $('.event-add').removeClass('event-area-block');
+    }
+    if (isCancel) {
+        clearInputValue()
+    }
+}
+
+function clearInputValue() {
+    const title = $('.event .type-title');
+    const date = $('.event input[type="date"]');
+    const time = $('.event input[type="time"]');
+    const comment = $('.event .comment-area');
+    title.value = '';
+    date.value = '';
+    time.value = '';
+    comment.value = '';
 }
