7dae907ef29d2d19d1fb1845837aaee126b72625
Larry <shaka422@hotmail.com>
2020-08-06 19:28:57 +0800

    增加各個function註解

--- Larry_T7.1/jQuery/module.js
+++ Larry_T7.1/jQuery/module.js
@@ -5,15 +5,14 @@ let currentTask = null;
 $.map(allTaskData, data => data.isEdit = false)
 
 render(sortData());
+
 $('.add-area').on('click', openNewTask);
-
-
-
 function openNewTask() {
     $('.add-area').addClass('add-area-none');
     $('.event-add').addClass('event-area-block');
 }
 
+//註冊監聽nav點擊事件
 $('.nav').on('click', $('.nav'), taskClassification);
 function taskClassification(e) {
     changeState(e)
@@ -52,11 +51,8 @@ function taskStateClassification() {
     }
 }
 
-//事件委派
+//註冊監聽新增任務事件
 $('.event').on('click', $('.event'), newEventBinding);
-// $('.task').addEventListener('dragstart', dragEventBinding)
-
-
 function newEventBinding(e) {
     const isStar = $(e.target).hasClass('top-star');
     const isEdit = $(e.target).hasClass('edit-pen');
@@ -119,6 +115,7 @@ function clearNewTaskData() {
     check.checked = false;
 
 }
+
 function removeEventClass() {
     $('.event').removeClass('high-light');
     $('.event').removeClass('is-complete');
@@ -126,6 +123,7 @@ function removeEventClass() {
     $('.event-add').removeClass('event-area-block');
 }
 
+//註冊監聽拖曳事件
 $.map($('.task'), task => {
     task.addEventListener('dragstart', dragTask)
 })
@@ -145,26 +143,34 @@ function cancelDefault(e) {
     e.preventDefault();
 }
 
+//拖曳事件
 function dragTask(e) {
+    //儲存被拖拉元素的 dataset 數值
     e.dataTransfer.setData('text/plain', this.dataset.number);
 }
-
+//拖曳後，放入位置
 function dropTask(e) {
+    //取得被拖拉元素的 dataset 數值
     const dataNumber = e.dataTransfer.getData('text/plain');
+    //利用取得拖拉元素的數值，反查對應的 DOM 元素
     const dragItem = $(`div[data-number="${dataNumber}"]`)[0];
-
+    //如果拖拉元素的父層容器與被放入元素的父層容器不同，就取消放入元素效果
     if (dragItem.parentElement !== this.parentElement) return
 
+    //如果被拖拉元素的下一個元素是當前擺放的位置
     if (dragItem.nextElementSibling === this) {
+        //將當前擺放位置的元素，插入被拖拉的元素上方
         this.parentElement.insertBefore(this, dragItem);
     } else {
+        //如果不是就將被拖拉元素，插入當前擺放位置的元素上方
         this.parentElement.insertBefore(dragItem, this);
     }
     saveRandomTask()
 }
-
+//儲存當前頁面排序
 function saveRandomTask() {
     const [...tasks] = document.querySelectorAll('.task');
+    //動態新增order並給予數值用於排序
     tasks.filter(taskFinish => taskFinish.classList.contains('is-complete'))
         .map(taskDone => taskDone.dataset.number)
         .forEach((taskDone, index) => {
@@ -190,7 +196,7 @@ function sortData() {
     const middle = allTaskData.filter(data => !data.isStar && !data.isComplete);
     const bottom = allTaskData.filter(data => data.isComplete);
     const getOrder = allTaskData.map(data => data.order).some(data => data !== null)
-
+    //如果allTaskData有order且不是null，使用以下排序
     if (getOrder) {
         const starSort = top.sort((a, b) => a.order - b.order);
         const middleSort = middle.sort((a, b) => a.order - b.order)
@@ -207,11 +213,13 @@ function sortData() {
             bottom: [...bottomSort]
         }
     }
+    //如果allTaskData，沒有order使用以下排序
     if (!getOrder) {
         const starSort = top.sort((a, b) => b.id - a.id);
         const middleSort = middle.sort((a, b) => b.id - a.id)
         const bottomSort = bottom.sort((a, b) => {
             if (a.isStar === b.isStar) {
+                //排序大到小
                 return b.id - a.id
             }
             a.isStar && !b.isStar ? -1 : 1;
@@ -225,6 +233,7 @@ function sortData() {
     }
 }
 
+//畫面渲染
 function render({ top, middle, bottom }) {
     const numberOfTask = $(document).find('.task').length;
     $('.star-area').html($.map(top, data => createList(data)).join(''));
@@ -235,6 +244,7 @@ function render({ top, middle, bottom }) {
     $('.task-left').text(`${numberOfTask} tasks left`);
 }
 
+//編輯任務事件綁定
 function editEventBinding(e) {
     const isStar = $(e.target).hasClass('top-star');
     const isEdit = $(e.target).hasClass('edit-pen');
@@ -248,26 +258,27 @@ function editEventBinding(e) {
     });
     const targetTask = allTaskData[targetDataIndex];
 
+    //如果點擊的是置頂按鈕
     if (isStar) {
         pushEditStar.call(this, targetTask);
     }
-
+    //如果點擊的是勾選完成任務按鈕
     if (isCheck[0] === $(e.target)[0]) {
         pushEditCheck.call(this, targetTask);
     }
-
+    //如果點擊的是儲存按鈕
     if (isSave) {
         pushEditSave.call(this, targetTask, targetDataIndex, isCheck);
     }
-
+    //如果點擊的是編輯按鈕
     if (isEdit) {
         pushEditTaskButton.call(this, targetTask);
     }
-
+    //如果點擊的是刪除按鈕
     if (isDelete) {
         pushEditDelete(targetDataIndex);
     }
-
+    //如果點擊的是取消按鈕
     if (isCancel) {
         pushEditCancel.call(this, targetTask)
     }
@@ -280,16 +291,20 @@ function pushEditCancel(targetTask) {
     render(sortData());
 }
 function pushEditStar(targetTask) {
+    //如果是編輯中，就toggle high-light
     if (targetTask.isEdit) {
         $(this).toggleClass('high-light');
     } else {
+        //如果不是編輯中，就儲存置頂狀態並渲染
         targetTask.isStar = !targetTask.isStar;
         localStorage.setItem('taskData', JSON.stringify(allTaskData));
         render(sortData());
     }
 }
 function pushEditCheck(targetTask) {
+    //如果不是編輯中
     if (!targetTask.isEdit) {
+        //直接儲存已完成狀態，並渲染畫面
         targetTask.isComplete = !targetTask.isComplete;
         localStorage.setItem('taskData', JSON.stringify(allTaskData));
         render(sortData());
@@ -309,12 +324,15 @@ function pushEditSave(targetTask, targetDataIndex, isCheck) {
         isComplete: targetTask.isComplete,
         id: targetTask.id
     }
+    //如果當前元素有 high-light class 更改置頂狀態為 true
     if ($(this).hasClass('high-light')) {
         taskData.isStar = true;
     }
+    //如果當前元素有被勾選 更改完成狀態為 true
     if (isCheck[0].checked) {
         taskData.isComplete = true;
     }
+    //儲存當筆資料，並渲染畫面
     allTaskData[targetDataIndex] = taskData;
     localStorage.setItem('taskData', JSON.stringify(allTaskData));
     render(sortData());
@@ -326,6 +344,7 @@ function pushEditTaskButton(targetTask) {
     const comment = $(this).find('.comment-area');
     //編輯時 tittle可修改
     title.disabled = !title.disabled;
+    //修改編輯中狀態
     targetTask.isEdit = !targetTask.isEdit;
     title.val(targetTask.title);
     date.val(targetTask.date);
@@ -335,11 +354,13 @@ function pushEditTaskButton(targetTask) {
     currentTask = targetTask.id;
     $(this).toggleClass('isEdit');
 
+    //如果當前編輯任務，不是自己就關閉 該 edit
     if (preTask && preTask !== currentTask) {
         $(`[data-number="${preTask}"]`).removeClass('isEdit');
     }
 }
 function pushEditDelete(targetDataIndex) {
+    //取得被選取的資料，使用splice刪除後，儲存資料並渲染畫面
     allTaskData.splice(targetDataIndex, 1)
     localStorage.setItem('taskData', JSON.stringify(allTaskData));
     render(sortData());
